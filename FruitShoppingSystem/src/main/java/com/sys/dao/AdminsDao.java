8dd91a5c8d3e9da39f021f5e8949080491a09c81
youziyou88 <88133015+youziyou88@users.noreply.github.com>
2021-07-31 13:55:51 +0800

    Add files via upload

--- FruitShoppingSystem/src/main/java/com/sys/dao/AdminsDao.java
+++ FruitShoppingSystem/src/main/java/com/sys/dao/AdminsDao.java
@@ -1,9 +1,4 @@
 package com.sys.dao;
 
-import com.sys.pojo.Admins;
-
-import java.util.List;
-
 public interface AdminsDao {
-    List<Admins> selectAdminById(int id);
 }
--- FruitShoppingSystem/src/main/java/com/sys/pojo/Goods.java
+++ FruitShoppingSystem/src/main/java/com/sys/pojo/Goods.java
@@ -1,4 +1,120 @@
 package com.sys.pojo;
 
+
+
 public class Goods {
+    private Integer gId;
+    private Integer mCertificatenum;
+    private String gName;
+    private Float gPrice;
+    private Integer gSum;
+    private String gPlcae;
+    private String gSize;
+    private Short gVIP;
+    private String gImage;
+
+
+    public Goods(Integer gId, Integer mCertificatenum, String gName, Float gPrice, Integer gSum, String gPlcae, String gSize, Short gVIP, String gImage) {
+        this.gId = gId;
+        this.mCertificatenum = mCertificatenum;
+        this.gName = gName;
+        this.gPrice = gPrice;
+        this.gSum = gSum;
+        this.gPlcae = gPlcae;
+        this.gSize = gSize;
+        this.gVIP = gVIP;
+        this.gImage = gImage;
+    }
+
+    @Override
+    public String toString() {
+        return "Goods{" +
+                "gId=" + gId +
+                ", mCertificatenum=" + mCertificatenum +
+                ", gName='" + gName + '\'' +
+                ", gPrice=" + gPrice +
+                ", gSum=" + gSum +
+                ", gPlcae='" + gPlcae + '\'' +
+                ", gSize='" + gSize + '\'' +
+                ", gVIP=" + gVIP +
+                ", gImage='" + gImage + '\'' +
+                '}';
+    }
+
+    public Integer getgId() {
+        return gId;
+    }
+
+    public void setgId(Integer gId) {
+        this.gId = gId;
+    }
+
+    public Integer getmCertificatenum() {
+        return mCertificatenum;
+    }
+
+    public void setmCertificatenum(Integer mCertificatenum) {
+        this.mCertificatenum = mCertificatenum;
+    }
+
+    public String getgName() {
+        return gName;
+    }
+
+    public void setgName(String gName) {
+        this.gName = gName;
+    }
+
+    public Float getgPrice() {
+        return gPrice;
+    }
+
+    public void setgPrice(Float gPrice) {
+        this.gPrice = gPrice;
+    }
+
+    public Integer getgSum() {
+        return gSum;
+    }
+
+    public void setgSum(Integer gSum) {
+        this.gSum = gSum;
+    }
+
+    public String getgPlcae() {
+        return gPlcae;
+    }
+
+    public void setgPlcae(String gPlcae) {
+        this.gPlcae = gPlcae;
+    }
+
+    public String getgSize() {
+        return gSize;
+    }
+
+    public void setgSize(String gSize) {
+        this.gSize = gSize;
+    }
+
+    public Short getgVIP() {
+        return gVIP;
+    }
+
+    public void setgVIP(Short gVIP) {
+        this.gVIP = gVIP;
+    }
+
+    public String getgImage() {
+        return gImage;
+    }
+
+    public void setgImage(String gImage) {
+        this.gImage = gImage;
+    }
+
+    public Goods() {
+
+
+    }
 }
